ca64d997ac37f6cf677d8985a810a94f64682a21
JonGoneWrong <jongonewrong@gmail.com>
2022-11-07 20:32:04 -0600

    Complete basic CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('applications/<int:pk>/', views.application_detail, name='detail'),
+    path('applications/<int:application_id>/',
+         views.application_detail, name='detail'),
     path('applications/create/', views.ApplicationCreate.as_view(),
          name='applications_create'),
     path('applications/<int:pk>/update/',
--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-# from django.urls import reverse
+from django.urls import reverse
 from .models import Application
 
 
@@ -41,7 +41,7 @@ class ApplicationUpdate(LoginRequiredMixin, UpdateView):
 
 class ApplicationDelete(LoginRequiredMixin, DeleteView):
     model = Application
-    success_url = "/home/"
+    success_url = "/"
 
 
 def signup(request):
